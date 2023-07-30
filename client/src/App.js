2fd77c52da474d0a04247d98909b93e33b1161b3
Marián Zelinka <maikelekmain@gmail.com>
2023-07-31 00:22:56 +0200

    ⚙Created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 
+import Index from "./pages/notLogged/Index"
+import Login from "./pages/notLogged/Login"
+import Register from "./pages/notLogged/Register"
+
+import Profile from "./pages/logged/Profile";
+import Decks from "./pages/logged/Decks";
+import Deck from "./pages/logged/Deck";
+import Review from "./pages/logged/Review";
+
 import "./App.css";
-
-
 
 function App() {
   return ( 
     <div className="App">
       <BrowserRouter>  
         <Routes>
+
+          <Route path="*" element={<Index/>}/>
+
+          <Route path="/" element={<Index/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/decks" element={<Decks/>}/>
+          <Route path="/deck/:id" element={<Deck/>}/>
+          <Route path="/review/:id" element={<Review/>}/>
+
+
 
         </Routes>
       </BrowserRouter>
