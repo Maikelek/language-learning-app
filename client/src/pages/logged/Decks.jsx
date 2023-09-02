85236b93e911c86b1168a5e5637153bdc55c037c
Marián Zelinka <maikelekmain@gmail.com>
2023-09-02 23:40:22 +0200

    😎 Decks Design

--- client/src/pages/logged/Decks.jsx
+++ client/src/pages/logged/Decks.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 
+import flag from "../../data/flags/flag_de.png"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus} from '@fortawesome/free-solid-svg-icons';
+
 const Decks = () => {
   return (
     <>
@@ -8,6 +13,17 @@ const Decks = () => {
         <Navbar />
 
         <div className="container">
+          
+          <div className="deck">
+            <div className="flag-holder">
+              <img src={flag} alt="flag" />
+            </div>
+            <h3>German</h3>
+          </div>
+
+          <div className='plus'>
+            <FontAwesomeIcon icon={faCirclePlus}/>
+          </div>
         </div>
 
     </>
