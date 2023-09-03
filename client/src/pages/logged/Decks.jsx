d8b46b8260c436284f7e8c17ae9a327a6b477e8d
Marián Zelinka <maikelekmain@gmail.com>
2023-09-03 23:46:57 +0200

    ⚙Edit & Remove button on hover

--- client/src/pages/logged/Decks.jsx
+++ client/src/pages/logged/Decks.jsx
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 
 import flag from "../../data/flags/flag_de.png"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlus} from '@fortawesome/free-solid-svg-icons';
+import { faCirclePlus,faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 const Decks = () => {
+  const [hovered, setHovered] = useState(false);
+
+  const handleHover = () => {
+    setHovered(true);
+  };
+
+  const handleUnhover = () => {
+    setHovered(false);
+  };
+
   return (
     <>
-    
-        <Navbar />
+      <Navbar />
 
-        <div className="container">
-          
-          <div className="deck">
-            <div className="flag-holder">
-              <img src={flag} alt="flag" />
+      <div className="container">
+
+        <div className="deck"
+             onMouseEnter={handleHover}
+             onMouseLeave={handleUnhover}>
+          <div className="flag-holder">
+            <img src={flag} alt="flag" />
+          </div>
+          <h3>German</h3>
+
+          {hovered && (
+            <div className='hovered-deck'>
+
+              <div>
+                <h3 style={{fontSize: "16px", marginBottom: "15px"}}>Update me</h3>
+              </div>
+
+              <div className="hovered-deck-center">
+                <button className='edit-button'>Edit <FontAwesomeIcon icon={faEdit}/></button>
+                <button className='remove-button'>Remove <FontAwesomeIcon icon={faTrash}/></button>
+              </div>
+
             </div>
-            <h3>German</h3>
-          </div>
-
-          <div className='plus'>
-            <FontAwesomeIcon icon={faCirclePlus}/>
-          </div>
+          )}
         </div>
 
+        <div className='plus'>
+            <FontAwesomeIcon icon={faCirclePlus} style={{cursor: "pointer"}}/>
+        </div>
+
+      </div>
     </>
   )
 }
