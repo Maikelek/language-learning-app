f2c81bdd3f13b51e1408667050283d691937f4bb
Marián Zelinka <maikelekmain@gmail.com>
2023-09-07 23:33:10 +0200

    ⚙React Add New Deck

--- client/src/pages/logged/Decks.jsx
+++ client/src/pages/logged/Decks.jsx
@@ -4,10 +4,27 @@ import Navbar from '../components/Navbar';
 import flag from "../../data/flags/flag_de.png"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlus,faTrash, faEdit, faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+import { faCirclePlus,faTrash, faEdit, faPuzzlePiece, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Decks = () => {
   const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const [newDeck, setNewDeck] = useState({
+    deck_name: '',
+    deck_flag: '',
+  });
+
+  const handleChange = (e) => {
+    setNewDeck((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = () => {
+    setClicked(true);
+  };
+
+  const handleUnclick = () => {
+    setClicked(false);
+  };
 
   const handleHover = () => {
     setHovered(true);
@@ -44,11 +61,46 @@ const Decks = () => {
           )}
         </div>
 
-        <div className='plus'>
+       
+
+        <div className='plus' onClick={handleClick}>
             <FontAwesomeIcon icon={faCirclePlus} style={{cursor: "pointer"}}/>
         </div>
 
       </div>
+
+      {clicked && (
+          <div className="add-deck">
+            <FontAwesomeIcon onClick={handleUnclick} icon={faCircleXmark} className='add-deck-ender'/>
+            
+            <form className='add-deck-form'>
+
+              <h3 style={{fontSize: "28px", marginBottom: "1.5rem", textAlign: "center"}}>Create a new deck</h3>
+
+                <div className="label-input">
+                  <label htmlFor="deck_name" className="label-classic">Name of the deck</label>
+                  <input 
+                    type="text" 
+                    id="deck_name" 
+                    name="deck_name" 
+                    placeholder='Insert deck name' 
+                    onChange={handleChange} 
+                    className="add-deck-input"
+                    />
+                </div>
+
+                <div className="label-input">
+                  <label htmlFor="deck_flag" className="label-classic">Flag</label>
+                  <select name="deck_flag" id="deck_flag" className="add-deck-select">
+                    <option value="flag_de">German</option>
+                  </select>
+                </div>
+
+            </form>
+
+          </div>
+        )}
+
     </>
   )
 }
